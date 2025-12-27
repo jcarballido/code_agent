4f2b334ba4ee74e88f35e9d903bcedceb75de1a2
Toast <carballidoj92@gmail.com>
2025-12-26 23:57:50 -0600

    Changed the pattern used to interface through the terminal.

--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,143 @@
 import readline from "node:readline"
-import askCoder from "./utils/askCoder.js"
-import validateFilePaths from "./validateFilePaths.js"
-import validateConstraints from "./validateConstraints.js"
 import requestProposal from "./utils/requestProposal.js"
+import type { Interface } from "node:readline"
+import type { ParseProposal } from "./types.js"
 
 console.log('Chat started. Enter "exit" to quit.')
 
-async function chat(){
-  const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-  })
-
-  // Initiate prompt
-  rl.question('Enter a prompt: ', async (input) => {
-    let promptRetries = 0
-    let isPromptValidated = false
-    const rejectionCause:{files:string[], constraints:string[]} = {
-      files:[],
-      constraints:[]
-    }
-    
-    if(input == 'exit'){
-      console.log('Chat ended.')
-      rl.close()
-      process.exit(0)
-    }
-    
-    while(promptRetries < 3 && !isPromptValidated){
-      try {
-        let updatedPrompt: string = ''
-        if(rejectionCause.constraints.length > 0 || rejectionCause.files.length > 0){
-          updatedPrompt += `
-          Your previous proposal was REJECTED for the following reasons:
-          `
-          if(rejectionCause.files.length > 0){
-            updatedPrompt += `
-            ${rejectionCause.files}
-            `
-          }
-          if(rejectionCause.constraints.length > 0){
-            updatedPrompt += `
-            ${rejectionCause.constraints}            
-            `
-          }
-        }else{
-          updatedPrompt += input
-        }
-
-        const validationResult = await requestProposal(updatedPrompt,rejectionCause)
-
-        if(rejectionCause.files.length == 0 && rejectionCause.constraints.length == 0){
-          console.log('Valid Porposal: ', validationResult)
-          // Show the proposal
-          // Ask for approval or rejection
-          rl.question('Do you approve this proposal? Agent will then generate code for the specified files. \n [y,n]', async (input) => {
-            if(input == 'yes' || input == 'y' || input == 'Yes' || input == 'Y'){
-              console.log('Proposal has been accepted.')
-              isPromptValidated = true
-            }
-          })
-          // If approval, break out of loop.
-          // If rejected, continue with retrying the prompt
-        }
-        promptRetries++ 
-
-      } catch (error) {
-        console.log('Error on prompt: ', error)
+function prompt(rl:Interface, greeting: string): Promise<string> {
+  return new Promise((resolve) => {
+    rl.question(greeting, (input) => {
+      if(input == 'exit'){
+        console.log('Chat ended.')
         rl.close()
         process.exit(0)
       }
-    }
-
-    console.log('Agent completed attempting to run task generation. Goodbye!')
-    rl.close()
-    process.exit(0)
-
-
+      resolve(input)      
+    })
   })
+}
 
+function prompt1(rl:Interface, greeting:string):Promise<ParseProposal> {
+  return new Promise((resolve) => {
+    rl.question(greeting, async (input) => {
+
+      if(input == 'exit'){
+        console.log('Chat ended.')
+        rl.close()
+        process.exit(0)
+      }
+
+      let promptRetries = 0
+      let isPromptValidated = false
+      const rejectionCause:{files:string[], constraints:string[]} = {
+        files:[],
+        constraints:[]
+      }      
+      while(promptRetries < 3 && !isPromptValidated){
+        console.log('While loop executing...')
+        console.log('Prompt retries: ', promptRetries)
+        try {
+          let updatedPrompt: string = ''
+          if(rejectionCause.constraints.length > 0 || rejectionCause.files.length > 0){
+            updatedPrompt += `
+            Your previous proposal was REJECTED for the following reasons:
+            `
+            if(rejectionCause.files.length > 0){
+              updatedPrompt += `
+              ${rejectionCause.files}
+              `
+            }
+            if(rejectionCause.constraints.length > 0){
+              updatedPrompt += `
+              ${rejectionCause.constraints}            
+              `
+            }
+          }else{
+            updatedPrompt += input
+          }
+
+          const validationResult = await requestProposal(updatedPrompt,rejectionCause)
+
+          if(rejectionCause.files.length == 0 && rejectionCause.constraints.length == 0){
+            console.log('Valid Porposal: ', validationResult)
+            resolve(validationResult)
+            isPromptValidated = true
+          }else{
+            promptRetries++ 
+          }
+        } catch (error) {
+          console.log('Error on prompt: ', error)
+          rl.close()
+          process.exit(0)
+        }
+      } 
+    })
+  })
+}
+
+async function chat(){
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  const input = await prompt(rl, 'What are we building?')
+  console.log('Input entered: ', input)
+
+  let promptRetries = 0
+  let isPromptValidated = false
+  const rejectionCause:{files:string[], constraints:string[]} = {
+    files:[],
+    constraints:[]
+  }      
+  let validatedProposal
+  while(promptRetries < 3 && !isPromptValidated){
+    console.log('While loop executing...')
+    console.log('Prompt retries: ', promptRetries)
+    try {
+      let updatedPrompt: string = ''
+      if(rejectionCause.constraints.length > 0 || rejectionCause.files.length > 0){
+        updatedPrompt += `
+        Your previous proposal was REJECTED for the following reasons:
+        `
+        if(rejectionCause.files.length > 0){
+          updatedPrompt += `
+          ${rejectionCause.files}
+          `
+        }
+        if(rejectionCause.constraints.length > 0){
+          updatedPrompt += `
+          ${rejectionCause.constraints}            
+          `
+        }
+      }else{
+        updatedPrompt += input
+      }
+
+      validatedProposal = await requestProposal(updatedPrompt,rejectionCause)
+
+      if(rejectionCause.files.length == 0 && rejectionCause.constraints.length == 0){
+        console.log('Valid Porposal: ', validatedProposal)
+        isPromptValidated = true
+      }else{
+        promptRetries++ 
+      }
+    } catch (error) {
+      console.log('Error on prompt: ', error)
+      rl.close()
+      process.exit(0)
+    }
+  } 
+
+  const check = await prompt(rl, 'Do you approve this proposal? [Y/N]')
+  if(check == 'y' ||check == 'Y' || check == 'Yes' || check == 'yes'){
+    console.log('Proposal accepted')
+  }else{
+    console.log('Proposal rejected')
+  }
+
+  rl.close()
+  console.log('Agent exiting process. Goodbye!')
 }
 
 chat()
